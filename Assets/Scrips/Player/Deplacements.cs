6bffb583263ee6f84fe7e3535088bea974eb3726
HANNJodie <117754974+HANNJodie@users.noreply.github.com>
2023-11-22 00:08:30 +0100

    Deplacements
    changement et amélioration des déplacements du joueur

--- Assets/Scrips/Player/Deplacements.cs
+++ Assets/Scrips/Player/Deplacements.cs
@@ -18,27 +18,20 @@ public class Deplacements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //var vDeplacement = Input.GetAxis("Vertical") * vitesseDeplacement;
-        //RigidbodyPerso.velocity = transform.forward * vDeplacement;
-        //RigidbodyPerso.velocity = (transform.forward * vDeplacement) + new Vector3(0, RigidbodyPerso.velocity.y, 0);
-        //RigidbodyPerso.velocity = new Vector3(0, 0, vDeplacement);
 
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            RigidbodyPerso.AddForce(0f, 0f, 1f * vitesseDeplacement);
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            RigidbodyPerso.AddForce(0f, 0f, -1f * vitesseDeplacement);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            RigidbodyPerso.AddForce(1f * vitesseDeplacement, 0f, 0f);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            RigidbodyPerso.AddForce(-1f * vitesseDeplacement, 0f, 0f);
-        }
+        // Calcul de la vitesse verticale
+
+        var vDeplacement = Input.GetAxis("Vertical") * vitesseDeplacement; 
+        RigidbodyPerso.velocity = transform.forward * vDeplacement;
+
+        // Calcul de la vitesse horizontale
+
+        var hDeplacement = Input.GetAxis("Horizontal") * vitesseDeplacement;
+        RigidbodyPerso.velocity = transform.forward * hDeplacement;
+
+        // Applique la vitesses
+
+        RigidbodyPerso.velocity = new Vector3(hDeplacement, 0, vDeplacement);
 
 
     }
